66b6942eddc7e66658904306dd34710204279ac1
andrija14 <andrija.zecevic098@gmail.com>
2024-11-30 15:48:10 +0100

    backend update

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'eshopovic-host.render.com',
     user: 'root',
     password: 'andrija2005',
     database: 'Eshop',
+    port: 3306
 });
 
 module.exports = db;
